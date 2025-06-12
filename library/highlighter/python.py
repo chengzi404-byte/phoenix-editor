df4a4e681a13a6022e98333c16a36436d3753850
chengzi404 <xxx202225@163.com>
2025-06-12 15:39:05 +0800

    chore: change comments of Python highlighter

--- library/highlighter/python.py
+++ library/highlighter/python.py
@@ -6,16 +6,16 @@ import keyword
 class CodeHighlighter(BaseHighlighter):
     def __init__(self, text_widget):
         super().__init__(text_widget)
-        # Python特定的内置函数和关键字
+
         self.builtins = set(dir(builtins))
         self.keywords = set(keyword.kwlist)
         
-        # Python特定的语法高亮规则
+        # Python syntax colors
         self.syntax_colors.update({
-            "f_string": self.syntax_colors["string"],     # f-string使用字符串颜色
-            "bytes": self.syntax_colors["string"],        # 字节字符串
-            "exception": self.syntax_colors["class"],     # 异常类使用类的颜色
-            "magic_method": self.syntax_colors["function"], # 魔术方法
+            "f_string": self.syntax_colors["string"],     # f-string (use the string color)
+            "bytes": self.syntax_colors["string"],        # byte-string
+            "exception": self.syntax_colors["class"],     # exception
+            "magic_method": self.syntax_colors["function"], # magic method
         })
         self.setup_tags()
         
@@ -28,20 +28,20 @@ class CodeHighlighter(BaseHighlighter):
             
         start, end = self.get_position(node)
         
-        # Python特定的节点处理
+
         if isinstance(node, ast.JoinedStr):  # f-string
             self._highlight_f_string(node, start, end)
-        elif isinstance(node, ast.Bytes):     # 字节字符串
+        elif isinstance(node, ast.Bytes):     # byte string
             self._add_tag("bytes", start, end)
-        elif isinstance(node, ast.Try):       # try-except 块
+        elif isinstance(node, ast.Try):       # try-except block
             self._highlight_try_except(node)
-        elif isinstance(node, ast.AsyncFunctionDef):  # 异步函数
+        elif isinstance(node, ast.AsyncFunctionDef):  # async function def
             self._highlight_async_function(node)
             
     def _highlight_f_string(self, node: ast.JoinedStr, start: str, end: str):
         """Highlight f-strings"""
         self._add_tag("f_string", start, end)
-        # 处理f-string中的表达式
+        # process f-string
         for value in node.values:
             if isinstance(value, ast.FormattedValue):
                 expr_start, expr_end = self.get_position(value)
@@ -49,7 +49,7 @@ class CodeHighlighter(BaseHighlighter):
                 
     def _highlight_try_except(self, node: ast.Try):
         """Highlight try-except blocks"""
-        # 高亮异常类
+        # try-except blocks
         for handler in node.handlers:
             if handler.type:
                 start, end = self.get_position(handler.type)
@@ -58,11 +58,11 @@ class CodeHighlighter(BaseHighlighter):
     def _highlight_async_function(self, node: ast.AsyncFunctionDef):
         """Highlight async functions"""
         start, end = self.get_position(node)
-        # 高亮async关键字
+        # highlight async keyword
         async_end = f"{node.lineno}.{node.col_offset + 5}"
         self._add_tag("keyword", start, async_end)
         
-        # 高亮函数名
-        name_start = f"{node.lineno}.{node.col_offset + 9}"  # async def 后的位置
+        # highlight function name
+        name_start = f"{node.lineno}.{node.col_offset + 9}"  # async def after
         name_end = f"{node.lineno}.{node.col_offset + 9 + len(node.name)}"
         self._add_tag("function", name_start, name_end)
