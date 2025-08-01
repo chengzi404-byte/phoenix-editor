9c88567c8dab25f1ebbdfd3ca64aca07ebe2661a
chengzi404 <xxx202225@163.com>
2025-08-01 19:18:55 +0800

    feat: remove useless files

--- library/execute/cpp.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-
-class CppExecuter(Executer):
-    def __init__(self, commandarea, printarea):
-        super().__init__(commandarea, printarea)
-        self.command = "g++ {} -o cache/cpp.exe -O2"
--- library/execute/python.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-
-class PythonExecuter(Executer):
-    def __init__(self, commandarea, printarea):
-        super().__init__(commandarea, printarea)
-        self.command = "python {}"
